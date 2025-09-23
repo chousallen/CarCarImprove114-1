495e33073dd9886b6546bc8af4faed35925114c9
sallen <chouallenno@gmail.com>
2025-09-23 22:25:45 +0800

    state switch in do routine

--- project/fsm.cpp
+++ project/fsm.cpp
@@ -12,6 +12,23 @@ void FSM::doRoutine()
 {
     // Placeholder for FSM routine logic
     readIR(ir_data);
+    switch(state) {
+        case STATE_NODE:
+            // Logic for NODE state
+            break;
+        case STATE_STRAIGHT:
+            // Logic for LINE_FOLLOW state
+            break;
+        case STATE_R_TURN:
+            // Logic for OBSTACLE_AVOIDANCE state
+            break;
+        case STATE_L_TURN:
+            // Logic for STOP state
+            break;
+        default:
+            // Handle unexpected state
+            break;
+    }
 }
 
 FSM_State FSM::getState()
